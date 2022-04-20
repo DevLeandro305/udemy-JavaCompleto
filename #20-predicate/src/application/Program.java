a97bc2a663e6486abfce3a93f9e23afda15a0c53
DevLeandro305 <leandro.soares@asempreceub.com>
2022-04-20 12:55:27 -0300

    Implementação por expressão lambda inline

--- #20-predicate/src/application/Program.java
+++ #20-predicate/src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -21,10 +20,8 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		double min = 100.0;		//Ou pode ser um valor digitado pelo usuário;
-		
-		Predicate<Product> pred = p -> p.getPrice() >= min;
-		
-		list.removeIf(pred);
+						
+		list.removeIf(p -> p.getPrice() >= min);
 		
 		for (Product p : list) {
 			System.out.println(p);
